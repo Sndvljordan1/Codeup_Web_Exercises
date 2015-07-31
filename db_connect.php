636ad954f2fe44b5327e013112ceda934c1fda44
Jordan Sandoval <sndvljordan@aol.com>
2015-07-31 14:21:21 -0500

    connecting mysql to php

---
+++ db_connect.php
@@ -0,0 +1,26 @@
+<?php 
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'sndvljordan');
+
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
